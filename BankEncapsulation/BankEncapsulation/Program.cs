49266202a4b61aa51dfb7a71fa5994d8815d7c61
cmreavis <cmreavis97@gmail.com>
2022-09-23 14:58:35 -0400

    First commit

---
+++ BankEncapsulation/BankEncapsulation/Program.cs
@@ -0,0 +1,17 @@
+﻿using BankEncapsulation;
+
+//New account
+BankAccount firstCustomer = new BankAccount(0);
+
+//Deposit funds
+firstCustomer.Deposit(40000);
+
+Console.WriteLine(firstCustomer.GetBalance());
+//Withdraw funds
+firstCustomer.Withdraw(3200);
+
+Console.WriteLine(firstCustomer.GetBalance());
+
+
+//Trying to directly print balance will result in an error due to access level
+//Console.WriteLine(firstCustomer.balance);
